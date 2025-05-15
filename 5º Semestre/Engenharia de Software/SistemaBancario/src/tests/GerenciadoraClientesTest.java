491f2e68af1a1ac25d228a49247eb5e71eaa53be
Eduardo Zirbell <eduardo.zirbell@lincros.com>
2025-05-15 13:13:12 -0300

    Atualizando trabalhos dia 15/05

---
+++ 5º Semestre/Engenharia de Software/SistemaBancario/src/tests/GerenciadoraClientesTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GerenciadoraClientesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getClientesDoBanco() {
+    }
+
+    @Test
+    void pesquisaCliente() {
+    }
+
+    @Test
+    void adicionaCliente() {
+    }
+
+    @Test
+    void removeCliente() {
+    }
+
+    @Test
+    void clienteAtivo() {
+    }
+
+    @Test
+    void limpa() {
+    }
+
+    @Test
+    void validaIdade() {
+    }
+}
---
+++ 5º Semestre/Engenharia de Software/SistemaBancario/src/tests/GerenciadoraContasTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class GerenciadoraContasTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getContasDoBanco() {
+    }
+
+    @Test
+    void pesquisaConta() {
+    }
+
+    @Test
+    void adicionaConta() {
+    }
+
+    @Test
+    void removeConta() {
+    }
+
+    @Test
+    void contaAtiva() {
+    }
+
+    @Test
+    void transfereValor() {
+    }
+}
